91f323ba7932c95454133b1a4308ba100cbed643
“sergio <sergiowork98@gmail.com>
2022-07-19 22:32:37 -0300

    agg cambios contenido

---
+++ app.js
@@ -0,0 +1,6 @@
+let edad = prompt("Ingresa tu edad");
+let edadNumerica = parseInt (edad);
+let fechaNacimiento = 2022 - edadNumerica;
+let mensaje = "Naciste en el año " + fechaNacimiento;
+alert(mensaje);
+console.log(mensaje); 
